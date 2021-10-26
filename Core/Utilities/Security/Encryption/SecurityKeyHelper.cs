9f1ac6c433c81f39d842da2927ab44fe6a4252a1
kayao <kayaoguzhan@windowslive.com>
2021-10-26 18:34:43 +0300

    26.10.2021 commit 2

---
+++ Core/Utilities/Security/Encryption/SecurityKeyHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Encryption
+{
+    public class SecurityKeyHelper
+    {
+        public static SecurityKey CreateSecurityKey(string securityKey)//appsettingdeki sexuritykey i byte array haline getiriyopr
+        {
+            return new SymmetricSecurityKey(Encoding.UTF8.GetBytes(securityKey));
+        }
+    }
+}
---
+++ Core/Utilities/Security/Encryption/SigningCredentialsHelper.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Utilities.Security.Encryption
+{
+    public class SigningCredentialsHelper
+    {
+        public static SigningCredentials CreateSigningCredentials(SecurityKey securityKey) ///web tokenların oluşturulması için
+        {
+            return new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256Signature);
+        }
+    }
+}
